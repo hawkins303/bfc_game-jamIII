d738e0c191220b12e0a1744fd0437e2fbc9accae
Nate <nathan.ronca@gmail.com>
2026-01-10 20:33:14 -0500

    adding player spawn point

--- player.py
+++ player.py
@@ -3,7 +3,8 @@ from ursina import *
 # Define player entity
 player = Entity(scale=.05, collider='box',
         move_dir=Vec3(0,0,0),   # Player movement direction
-        speed = 0.5             # Player move speed
+        speed = 0.5,             # Player move speed
+        position=Vec3(-0.42, -0.32, 0) # Player spawn point
     )
 p_sprite = SpriteSheetAnimation(parent=player, texture="/assets/nancy_sprite.png", tileset_size=[6,6], fps= 4,
                                 animations={'idle': ((0, 5), (1, 5)), 'walk_down': ((0, 5), (3, 5)),
